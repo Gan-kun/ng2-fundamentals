7684d1f17e889d5851d9ea00045aaed3c1549495
Akang Kwik <akangkwik@myalias.com>
2017-10-16 18:11:18 +0700

    Ch03.02. First Data-Bound Component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,15 @@
-import  { NgModule } from '@angular/core'
-import  { BrowserModule } from '@angular/platform-browser'
-import  { EventsAppComponent } from './event-app.component'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { EventsAppComponent } from './event-app.component'
+import { EventsListComponent } from './events/events-list.component'
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent],
+    declarations: [
+        EventsAppComponent,
+        EventsListComponent
+    ],
     bootstrap: [EventsAppComponent],
 })
 
